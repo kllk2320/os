8a5cd4c62348f2e242211a6c248e60a683c3f0c9
Liu Kui <kllk2320@gmail.com>
2015-10-02 16:50:03 +0800

    Output with framebuff

---
+++ l4-output/fb.c
@@ -0,0 +1,35 @@
+
+enum {
+    FB_BLACK = 0,
+    FB_BLUE,
+    FB_GREEN,
+    FB_CYAN,
+    FB_RED,
+    FB_MAGENTA,
+    FB_BROWN,
+    FB_LIGHT_GREY,
+    FB_DARK_GREY,
+    FB_LIGHT_BLUE,
+    FB_LIGHT_GREEN,
+    FB_LIGHT_CYAN,
+    FB_LIGHT_RED,
+    FB_LIGHT_MAGENTA,
+    FB_LIGHT_BROWN,
+    FB_WHITE,
+};
+
+
+
+char *fb = (char *) 0x000b8000;
+
+/** fb_write_cell
+ *  Writes a character with the given foreground and background to position i in the framebuffer
+ */
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
+{
+    fb[i] = c;
+    fb[i + 1] = ((fg & 0x0f) << 4) | (bg &0x0f);
+}
+
+
+
